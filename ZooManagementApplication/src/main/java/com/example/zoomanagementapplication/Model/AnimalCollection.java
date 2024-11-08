acfd16742dd658339e452c064ead2193b3eba577
Anarka666 <avchetty@hotmail.com>
2024-11-08 10:04:05 -0500

    created Interface

--- ZooManagementApplication/src/main/java/com/example/zoomanagementapplication/Model/AnimalCollection.java
+++ ZooManagementApplication/src/main/java/com/example/zoomanagementapplication/Model/AnimalCollection.java
@@ -1,4 +1,18 @@
 package com.example.zoomanagementapplication.Model;
 
+import java.util.List;
+
 public interface AnimalCollection {
+    // Adds an animal to the collection (could be an Enclosure or a CompositeAnimalCollection)
+    void addAnimal(Animal animal);
+
+    // Removes an animal from the collection
+    void removeAnimal(Animal animal);
+
+    // Displays the list of animals in the collection
+    void displayAnimals();
+
+    // Returns a list of all animals in the collection
+    List<Animal> getAnimals();
 }
+
